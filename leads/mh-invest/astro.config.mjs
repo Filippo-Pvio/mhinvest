2b2a1856a7b077516c19242afc7dbc7b00efd235
Filippo-Pvio <f.livera@sls.de>
2026-09-20 00:05:54 +0200

    Fix GitHub Pages base paths

--- leads/mh-invest/astro.config.mjs
+++ leads/mh-invest/astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://filippo-pvio.github.io',
+  base: '/mhinvest',
   output: 'static',
   build: {
     inlineStylesheets: 'auto'
